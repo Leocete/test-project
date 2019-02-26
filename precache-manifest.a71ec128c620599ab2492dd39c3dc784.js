4152289633f340d31142ff56b35d8449449fbd87
Leocete <37488015+Leocete@users.noreply.github.com>
2019-02-26 18:27:02 +0200

    Updates

---
+++ precache-manifest.a71ec128c620599ab2492dd39c3dc784.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b594a7113d6455c9a95",
+    "url": "/react-test-app/static/js/runtime~main.5b594a71.js"
+  },
+  {
+    "revision": "356baa80ce02bc503e2b",
+    "url": "/react-test-app/static/js/main.356baa80.chunk.js"
+  },
+  {
+    "revision": "5cb9b55e9859431b45ee",
+    "url": "/react-test-app/static/js/1.5cb9b55e.chunk.js"
+  },
+  {
+    "revision": "356baa80ce02bc503e2b",
+    "url": "/react-test-app/static/css/main.30d39824.chunk.css"
+  },
+  {
+    "revision": "fd61b4c613aa7fb5b5196a3364026796",
+    "url": "/react-test-app/index.html"
+  }
+];
